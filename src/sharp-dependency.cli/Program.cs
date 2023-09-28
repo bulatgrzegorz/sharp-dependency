ccf33314dc9a00df65ae95691bf44299e605531a
bulatgrzegorz <grzegorz0bulat@gmail.com>
2023-09-28 21:17:44 +0200

    cli start of update repository method

--- src/sharp-dependency.cli/Program.cs
+++ src/sharp-dependency.cli/Program.cs
@@ -4,33 +4,166 @@ using sharp_dependency;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
+foreach (var a in Enum.GetValues<Environment.SpecialFolder>())
+{
+    Console.WriteLine($"{Enum.GetName(a)}: {Environment.GetFolderPath(a)}");
+}
+
+
 var app = new CommandApp();
 app.Configure(config =>
 {
     config.UseStrictParsing();
-    config.AddCommand<UpdateDependencyCommand>("update");
+    config.AddBranch("update", x =>
+    {
+        x.AddCommand<UpdateLocalDependencyCommand>("local");
+        x.AddCommand<UpdateRepositoryDependencyCommand>("repo");
+    });
+    // config.AddCommand<UpdateDependencyCommand>("update local");
+    // config.AddCommand<UpdateDependencyCommand>("update repo");
 });
 
 await app.RunAsync(args);
 
+internal sealed class UpdateRepositoryDependencyCommand : AsyncCommand<UpdateRepositoryDependencyCommand.Settings>
+{
+    private readonly BitbucketRepositoryManager _bitbucketRepository;
+    
+    public sealed class Settings : CommandSettings
+    {
+        [CommandOption("-r|--repository")]
+        public string? Repository { get; init; }
+        
+        [CommandOption("-p|--project")]
+        public string? Project { get; init; }
+    }
+
+    public UpdateRepositoryDependencyCommand()
+    {
+        var configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddUserSecrets<Program>();
+
+        var configuration = configurationBuilder.Build();
+        
+        var nugetManager = new NugetPackageSourceMangerChain(
+            new NugetPackageSourceManger(),
+            new NugetPackageSourceManger(configuration["NugetAddress"]!, NugetPackageSourceManger.ApiVersion.V3, (configuration["NugetUserName"]!, configuration["NugetToken"]!, true)));
+
+        
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
+    {
+        var configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddUserSecrets<Program>();
+        var configuration = configurationBuilder.Build();
+
+        var nugetManager = new NugetPackageSourceMangerChain(
+            new NugetPackageSourceManger(),
+            new NugetPackageSourceManger(configuration["NugetAddress"]!, NugetPackageSourceManger.ApiVersion.V3, (configuration["NugetUserName"]!, configuration["NugetToken"]!, true)));
+
+        var bitbucketManager = new BitbucketRepositoryManager(
+            configuration["BitbucketAddress"]!,
+            configuration["BitbucketToken"]!, 
+            settings.Repository, 
+            settings.Project);
+
+        var repositoryPaths = (await bitbucketManager.GetRepositoryFilePaths()).ToList();
+
+        var projectPaths = await GetProjectPaths(repositoryPaths, bitbucketManager);
+        foreach (var projectPath in projectPaths)
+        {
+            Console.WriteLine("{0}", projectPath);
+
+            var projectContent = await bitbucketManager.GetFileContentRaw(projectPath);
+            await using var projectFileParser = new ProjectFileParser(projectContent);
+            var projectFile = await projectFileParser.Parse();
+            foreach (var dependency in projectFile.Dependencies)
+            {
+                var allVersions = await nugetManager.GetPackageVersions(dependency.Name, projectFile.TargetFrameworks);
+                if (allVersions.Count == 0)
+                {
+                    continue;
+                }
+
+                if (dependency.UpdateVersionIfPossible(allVersions, out var newVersion))
+                {
+                    Console.WriteLine("     {0} {1} -> {2}", dependency.Name, dependency.CurrentVersion, newVersion);    
+                }
+            }
+            
+            var updatedProjectContent = await projectFileParser.Generate();
+            
+            // if (!settings.DryRun)
+            // {
+            //     await File.WriteAllTextAsync(projectPath, updatedProjectContent);
+            // }
+        }
+        
+        return 0;
+    }
+
+    private async Task<IReadOnlyCollection<string>> GetProjectPaths(IReadOnlyCollection<string> filePaths, BitbucketRepositoryManager bitbucketManager)
+    {
+        var solutionsPath = FindSolutionPath(filePaths);
+        if (solutionsPath is null) return filePaths.Where(FileIsProjectFile).ToList();
+        
+        var solutionParser = new SolutionFileParser();
+        var solutionContent = await bitbucketManager.GetFileContent(solutionsPath);
+        return solutionParser.GetProjectPaths(solutionContent);
+    }
+
+    private string? FindSolutionPath(IReadOnlyCollection<string> filePaths)
+    {
+        var solutionsPaths = FindSolutionFiles(filePaths);
+        if (solutionsPaths.Count == 0)
+        {
+            return null;
+        }
+
+        return FindSolutionPathSrcLevel(solutionsPaths) ?? FindSolutionPathRootLevel(solutionsPaths) ?? solutionsPaths.First();
+    }
+
+    private IReadOnlyCollection<string> FindSolutionFiles(IReadOnlyCollection<string> filePaths)
+    {
+        return filePaths.Where(FileIsSln).ToList();
+    }
+
+    private string? FindSolutionPathRootLevel(IReadOnlyCollection<string> solutionPaths)
+    {
+        return solutionPaths.FirstOrDefault(DirectoryIsEmpty);
+    }
+    
+    private string? FindSolutionPathSrcLevel(IReadOnlyCollection<string> solutionPaths)
+    {
+        return solutionPaths.FirstOrDefault(DirectoryIsSrc);
+    }
+
+    private static bool DirectoryIsEmpty(string path) => string.IsNullOrEmpty(Path.GetDirectoryName(path));
+    private static bool DirectoryIsSrc(string path) => Path.GetDirectoryName(path)?.Equals("src", StringComparison.InvariantCultureIgnoreCase) ?? false;
+    private static bool FileIsProjectFile(string path) => path.EndsWith(".csproj", StringComparison.InvariantCultureIgnoreCase);
+    private static bool FileIsSln(string path) => path.EndsWith(".sln", StringComparison.InvariantCultureIgnoreCase);
+}
+
 // ReSharper disable once ClassNeverInstantiated.Global
-internal sealed class UpdateDependencyCommand : AsyncCommand<UpdateDependencyCommand.UpdateDependencySettings>
+// ReSharper disable once UnusedAutoPropertyAccessor.Global
+internal sealed class UpdateLocalDependencyCommand : AsyncCommand<UpdateLocalDependencyCommand.Settings>
 {
     // ReSharper disable once ClassNeverInstantiated.Global
-    public sealed class UpdateDependencySettings : CommandSettings 
+    public sealed class Settings : CommandSettings 
     {
         [Description("Path to solution/csproj which dependency should be updated")]
         [CommandArgument(0, "[path]")]
         public string? Path { get; init; }
-        
+
         [Description("Command will determine dependencies to be updated without actually updating them.")]
         [CommandOption("--dry-run")]
-        public bool DryRun { get; set; }
+        public bool DryRun { get; init; }
 
         public bool IsPathSolutionFile => Path!.EndsWith(".sln", StringComparison.InvariantCultureIgnoreCase);
     }
 
-    public override async Task<int> ExecuteAsync(CommandContext context, UpdateDependencySettings settings)
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
     {
         var configurationBuilder = new ConfigurationBuilder();
         configurationBuilder.AddUserSecrets<Program>();
@@ -75,15 +208,15 @@ internal sealed class UpdateDependencyCommand : AsyncCommand<UpdateDependencyCom
         return 0;
     }
 
-    private IReadOnlyCollection<string> GetProjectsPath(UpdateDependencySettings settings)
+    private IReadOnlyCollection<string> GetProjectsPath(Settings settings)
     {
         if (!settings.IsPathSolutionFile) return new[] { settings.Path! };
         
         var solutionFileParser = new SolutionFileParser();
-        return solutionFileParser.GetProjectPaths(FileContent.Create(settings.Path!));
+        return solutionFileParser.GetProjectPaths(FileContent.CreateFromLocalPath(settings.Path!));
     }
 
-    public override ValidationResult Validate(CommandContext context, UpdateDependencySettings settings)
+    public override ValidationResult Validate(CommandContext context, Settings settings)
     {
         if (string.IsNullOrWhiteSpace(settings.Path))
         {
--- src/sharp-dependency/Program.cs
+++ src/sharp-dependency/Program.cs
@@ -60,74 +60,6 @@ interface IRepositoryManger
     Task CreatePullRequest(string sourceBranch, string targetBranch, string prName, string description);
 }
 
-//TODO: Need also think about determining if pull request for some specific change was already created and we do not need to create another one.
-//There is also possibility that pull request should be updated, because another dependency update has been found 
-class BitbucketRepositoryManager : IRepositoryManger
-{
-    private readonly HttpClient _httpClient;
-    private const int PathsLimit = 1000;
-
-    public BitbucketRepositoryManager(string baseUrl, string authorizationToken, string repositoryName, string projectName)
-    {
-        _httpClient = new HttpClient(){BaseAddress = new Uri($"{baseUrl}/rest/api/latest/projects/{projectName}/repos/{repositoryName}/")};
-        _httpClient.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse($"Bearer {authorizationToken}");
-        //methods that accept multipart/form-data will only process requests with X-Atlassian-Token: no-check header.
-        _httpClient.DefaultRequestHeaders.Add("X-Atlassian-Token", "no-check");
-    }
-
-    public async Task<IEnumerable<string>> GetRepositoryFilePaths()
-    {
-        //TODO: We should make those as long as there are still files to be collected
-        var response = await _httpClient.GetFromJsonAsync<GetRepositoryFilePathsResponse>($"files?limit={PathsLimit}");
-        return response?.Values ?? Enumerable.Empty<string>();
-    }
-
-    public async Task<FileContent> GetFileContent(string filePath)
-    {
-        var response = await _httpClient.GetFromJsonAsync<GetFileContentResponse>($"browse/{filePath}");
-        return new FileContent(response?.Lines.Select(x => x.Text) ?? Enumerable.Empty<string>(), filePath);
-    }
-    
-    public async Task<Commit> EditFile(string branch, string commitMessage, string content, string filePath)
-    {
-        using var request = new MultipartFormDataContent();
-        request.Add(new StringContent(branch), "branch");
-        request.Add(new StringContent(commitMessage), "message");
-        request.Add(new StringContent(content), "content");
-        
-        var response = await _httpClient.PutAsync($"browse/{filePath}", request);
-        response.EnsureSuccessStatusCode();
-
-        var editFileResponse = await response.Content.ReadFromJsonAsync<EditFileResponse>();
-        return new Commit(){ Id = editFileResponse?.Id ?? string.Empty };
-    }
-
-    public Task CreatePullRequest(string sourceBranch, string targetBranch, string prName, string description)
-    {
-        throw new NotImplementedException();
-    }
-    
-    private class GetRepositoryFilePathsResponse
-    {
-        public IEnumerable<string> Values { get; set; }
-    }
-    
-    private class GetFileContentResponse
-    {
-        public IEnumerable<GetFileContentResponseLine> Lines { get; set; }
-
-        internal class GetFileContentResponseLine
-        {
-            public string Text { get; set; }
-        }
-    }
-    
-    private class EditFileResponse
-    {
-        public string Id { get; set; }
-    }
-}
-
 public class ProjectFile
 {
     public ProjectFile(IReadOnlyCollection<Dependency> dependencies, IReadOnlyCollection<string> targetFrameworks)
@@ -148,16 +80,16 @@ public class FileContent
         Path = path;
     }
 
-    public static FileContent Create(string path)
+    public static FileContent CreateFromLocalPath(string path)
     {
         return new FileContent(File.ReadAllLines(path), path);
-    } 
+    }
     
     public IEnumerable<string> Lines { get; private set; }
     public string Path { get; private set; }
 }
 
-internal class Commit
+public class Commit
 {
     public string Id { get; set; }
 }
