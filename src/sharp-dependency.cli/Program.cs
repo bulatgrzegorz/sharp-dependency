f6a6939e6b560dcaf28b133797989ae0373c0730
Grzegorz.Bulat <Grzegorz.Bulat@GAINCapital.com>
2023-09-28 16:44:36 +0200

    cli update local method

--- src/sharp-dependency.cli/Program.cs
+++ src/sharp-dependency.cli/Program.cs
@@ -8,23 +8,29 @@ var app = new CommandApp();
 app.Configure(config =>
 {
     config.UseStrictParsing();
-    config.AddCommand<UpdateSolutionDependencyCommand>("update");
+    config.AddCommand<UpdateDependencyCommand>("update");
 });
 
 await app.RunAsync(args);
 
 // ReSharper disable once ClassNeverInstantiated.Global
-internal sealed class UpdateSolutionDependencyCommand : AsyncCommand<UpdateSolutionDependencyCommand.UpdateSolutionDependencySettings>
+internal sealed class UpdateDependencyCommand : AsyncCommand<UpdateDependencyCommand.UpdateDependencySettings>
 {
     // ReSharper disable once ClassNeverInstantiated.Global
-    public sealed class UpdateSolutionDependencySettings : CommandSettings 
+    public sealed class UpdateDependencySettings : CommandSettings 
     {
-        [Description("Path to solution which dependency should be updated")]
-        [CommandArgument(0, "[solutionPath]")]
-        public string? SolutionPath { get; init; }
+        [Description("Path to solution/csproj which dependency should be updated")]
+        [CommandArgument(0, "[path]")]
+        public string? Path { get; init; }
+        
+        [Description("Command will determine dependencies to be updated without actually updating them.")]
+        [CommandOption("--dry-run")]
+        public bool DryRun { get; set; }
+
+        public bool IsPathSolutionFile => Path!.EndsWith(".sln", StringComparison.InvariantCultureIgnoreCase);
     }
 
-    public override async Task<int> ExecuteAsync(CommandContext context, UpdateSolutionDependencySettings settings)
+    public override async Task<int> ExecuteAsync(CommandContext context, UpdateDependencySettings settings)
     {
         var configurationBuilder = new ConfigurationBuilder();
         configurationBuilder.AddUserSecrets<Program>();
@@ -34,16 +40,14 @@ internal sealed class UpdateSolutionDependencyCommand : AsyncCommand<UpdateSolut
         var nugetManager = new NugetPackageSourceMangerChain(
             new NugetPackageSourceManger(),
             new NugetPackageSourceManger(configuration["NugetAddress"]!, NugetPackageSourceManger.ApiVersion.V3, (configuration["NugetUserName"]!, configuration["NugetToken"]!, true)));
-        var solutionFileParser = new SolutionFileParser();
-        var solutionProjects = solutionFileParser.GetProjectPaths(FileContent.Create(settings.SolutionPath!));
 
-        Console.WriteLine("Found {0} projects in solution {1}", solutionProjects.Count, settings.SolutionPath);
-        
-        foreach (var solutionProject in solutionProjects)
+        var projectPaths = GetProjectsPath(settings);
+
+        foreach (var projectPath in projectPaths)
         {
-            Console.WriteLine("{0}", solutionProject);
+            Console.WriteLine("{0}", projectPath);
             
-            var projectContent = await File.ReadAllTextAsync(solutionProject);
+            var projectContent = await File.ReadAllTextAsync(projectPath);
             await using var projectFileParser = new ProjectFileParser(projectContent);
             var projectFile = await projectFileParser.Parse();
             foreach (var dependency in projectFile.Dependencies)
@@ -61,17 +65,35 @@ internal sealed class UpdateSolutionDependencyCommand : AsyncCommand<UpdateSolut
             }
             
             var updatedProjectContent = await projectFileParser.Generate();
-            await File.WriteAllTextAsync(solutionProject, updatedProjectContent);
+            
+            if (!settings.DryRun)
+            {
+                await File.WriteAllTextAsync(projectPath, updatedProjectContent);
+            }
         }
 
         return 0;
     }
 
-    public override ValidationResult Validate(CommandContext context, UpdateSolutionDependencySettings settings)
+    private IReadOnlyCollection<string> GetProjectsPath(UpdateDependencySettings settings)
     {
-        if (string.IsNullOrWhiteSpace(settings.SolutionPath))
+        if (!settings.IsPathSolutionFile) return new[] { settings.Path! };
+        
+        var solutionFileParser = new SolutionFileParser();
+        return solutionFileParser.GetProjectPaths(FileContent.Create(settings.Path!));
+    }
+
+    public override ValidationResult Validate(CommandContext context, UpdateDependencySettings settings)
+    {
+        if (string.IsNullOrWhiteSpace(settings.Path))
         {
-            return ValidationResult.Error($"Setting {nameof(settings.SolutionPath)} must have a value.");
+            return ValidationResult.Error($"Setting {nameof(settings.Path)} must have a value.");
+        }
+
+        if (!settings.Path.EndsWith(".sln", StringComparison.InvariantCultureIgnoreCase) &&
+            !settings.Path.EndsWith(".csproj", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return ValidationResult.Error($"Setting {nameof(settings.Path)} must be either solution (sln) or project (csproj) file.");
         }
         
         return ValidationResult.Success();
