2f7ac531ea3f3c5f3809a418a276fa9910bb3756
bulatgrzegorz <grzegorz0bulat@gmail.com>
2023-10-04 21:12:40 +0200

    test for bitbucket cloud file paths

--- tests/sharp-dependency.UnitTests/BitbucketCloudRepositoryManagerTests.cs
+++ tests/sharp-dependency.UnitTests/BitbucketCloudRepositoryManagerTests.cs
@@ -58,6 +58,30 @@ public class BitbucketCloudRepositoryManagerTests
         Assert.Equal(responseContent, content);
     }
 
+    [Fact]
+    public async Task GetRepositoryFilePaths_WillCollectAllPaths()
+    {
+        var mockHttp = new MockHttpMessageHandler();
+        var baseAddress = "https://example.com/";
+
+        var firstLevelResponse = CreateSrcResponse(("link/file", "commit_file", "https://link/file"), ("proj", "commit_directory", "https://example.com/proj"));
+        mockHttp
+            .When($"{baseAddress}src")
+            .Respond(HttpStatusCode.OK, JsonContent.Create(firstLevelResponse));
+
+        var secondLevelResponse = CreateSrcResponse("proj/file1", "commit_file", "https://example.com/proj/file1");
+        mockHttp
+            .When("https://example.com/proj")
+            .Respond(HttpStatusCode.OK, JsonContent.Create(secondLevelResponse));
+
+        var httpClient = mockHttp.ToHttpClient();
+        httpClient.BaseAddress = new Uri(baseAddress);
+        var manager = new BitbucketCloudRepositoryManager(httpClient);
+        var paths = await manager.GetRepositoryFilePaths();
+        
+        Assert.Equal(new[]{"link/file", "proj/file1"}, paths);
+    }
+
     private static BitbucketCloudRepositoryManager.GetSrcResponse CreateSrcResponse(string path, string type, string link)
     {
         return new BitbucketCloudRepositoryManager.GetSrcResponse()
@@ -73,6 +97,40 @@ public class BitbucketCloudRepositoryManagerTests
                         Self = new BitbucketCloudRepositoryManager.GetSrcResponse.Self()
                         {
                             Href = link
+                        }
+                    }
+                }
+            }
+        };
+    }
+    
+    private static BitbucketCloudRepositoryManager.GetSrcResponse CreateSrcResponse((string path, string type, string link) val1, (string path, string type, string link) val2)
+    {
+        return new BitbucketCloudRepositoryManager.GetSrcResponse()
+        {
+            Values = new List<BitbucketCloudRepositoryManager.GetSrcResponse.Value>()
+            {
+                new()
+                {
+                    Path = val1.path,
+                    Type = val1.type,
+                    Links = new BitbucketCloudRepositoryManager.GetSrcResponse.Value.Link()
+                    {
+                        Self = new BitbucketCloudRepositoryManager.GetSrcResponse.Self()
+                        {
+                            Href = val1.link
+                        }
+                    }
+                },
+                new()
+                {
+                    Path = val2.path,
+                    Type = val2.type,
+                    Links = new BitbucketCloudRepositoryManager.GetSrcResponse.Value.Link()
+                    {
+                        Self = new BitbucketCloudRepositoryManager.GetSrcResponse.Self()
+                        {
+                            Href = val2.link
                         }
                     }
                 }
