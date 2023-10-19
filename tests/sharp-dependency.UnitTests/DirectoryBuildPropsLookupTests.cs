f5ae3d756a23c999149fc66bfc0faf692eaade3c
bulatgrzegorz <grzegorz0bulat@gmail.com>
2023-10-19 21:18:26 +0200

    directory.build.props tests

--- tests/sharp-dependency.UnitTests/DirectoryBuildPropsLookupTests.cs
+++ tests/sharp-dependency.UnitTests/DirectoryBuildPropsLookupTests.cs
@@ -6,7 +6,7 @@ public class DirectoryBuildPropsLookupTests
     [InlineData("tests/testProj/testProj.csproj", "tests/Directory.Build.props")]
     [InlineData("tool/tool.csproj", "Directory.Build.props")]
     [InlineData("src/proj/project.csproj", "src/proj/Directory.Build.props")]
-    public void Test(string projectPath, string expectedDirectoryBuildPropsFilePath)
+    public void DirectoryBuildPropsLookup_WillReturnPropsFile_ForRelativePaths_GnuLike(string projectPath, string expectedDirectoryBuildPropsFilePath)
     {
         var paths = new[]
         {
@@ -26,10 +26,60 @@ public class DirectoryBuildPropsLookupTests
     }
     
     [Theory]
+    [InlineData(@"tests\testProj\testProj.csproj", @"tests\Directory.Build.props")]
+    [InlineData(@"tool\tool.csproj", @"Directory.Build.props")]
+    [InlineData(@"src\proj\project.csproj", @"src\proj\Directory.Build.props")]
+    public void DirectoryBuildPropsLookup_WillReturnPropsFile_ForRelativePaths_WindowsLike(string projectPath, string expectedDirectoryBuildPropsFilePath)
+    {
+        var paths = new[]
+        {
+            @"project.sln",
+            @"Directory.Build.props",
+            @"src\Directory.Build.props",
+            @"src\proj\project.csproj",
+            @"src\proj\Directory.Build.props",
+            @"tool\tool.csproj",
+            @"tests\Directory.Build.props",
+            @"tests\testProj\testProj.csproj"
+        };
+
+        var directoryBuildProps = DirectoryBuildPropsLookup.GetDirectoryBuildPropsPath(paths, projectPath);
+        
+        Assert.Equal(expectedDirectoryBuildPropsFilePath, directoryBuildProps);
+    }
+    
+    [Fact]
+    public void DirectoryBuildPropsLookup_WillReturnNullForSingleProjectAtRoot_RelativePath()
+    {
+        var paths = new[]
+        {
+            "project.csproj"
+        };
+
+        var directoryBuildProps = DirectoryBuildPropsLookup.GetDirectoryBuildPropsPath(paths, "project.csproj");
+        
+        Assert.Null(directoryBuildProps);
+    }
+    
+    [Fact]
+    public void DirectoryBuildPropsLookup_WillReturnNullForSingleProjectInNestedDir_RelativePath()
+    {
+        var paths = new[]
+        {
+            "project.sln",
+            "proj/project.csproj"
+        };
+
+        var directoryBuildProps = DirectoryBuildPropsLookup.GetDirectoryBuildPropsPath(paths, "proj/project.csproj");
+        
+        Assert.Null(directoryBuildProps);
+    }
+    
+    [Theory]
     [InlineData(@"C:\dir\tests\testProj\testProj.csproj", @"C:\dir\tests\Directory.Build.props")]
     [InlineData(@"C:\dir\tool\tool.csproj", @"C:\dir\Directory.Build.props")]
     [InlineData(@"C:\dir\src\proj\project.csproj", @"C:\dir\src\proj\Directory.Build.props")]
-    public void Test1(string projectPath, string expectedDirectoryBuildPropsFilePath)
+    public void DirectoryBuildPropsLookup_WillReturnPropsFile_ForAbsolutePaths_WindowsLike(string projectPath, string expectedDirectoryBuildPropsFilePath)
     {
         var paths = new[]
         {
@@ -44,6 +94,56 @@ public class DirectoryBuildPropsLookupTests
         };
 
         var directoryBuildProps = DirectoryBuildPropsLookup.GetDirectoryBuildPropsPath(paths, projectPath, @"C:\dir");
+        
+        Assert.Equal(expectedDirectoryBuildPropsFilePath, directoryBuildProps);
+    }
+    
+    [Fact]
+    public void DirectoryBuildPropsLookup_WillReturnNullForSingleProjectAtRoot_AbsolutePath()
+    {
+        var paths = new[]
+        {
+            @"C:\dir\project.csproj"
+        };
+
+        var directoryBuildProps = DirectoryBuildPropsLookup.GetDirectoryBuildPropsPath(paths, @"C:\dir\project.csproj");
+        
+        Assert.Null(directoryBuildProps);
+    }
+    
+    [Fact]
+    public void DirectoryBuildPropsLookup_WillReturnNullForSingleProjectInNestedDir_AbsolutePath()
+    {
+        var paths = new[]
+        {
+            @"C:\dir\project.sln",
+            @"C:\dir\proj\project.csproj"
+        };
+
+        var directoryBuildProps = DirectoryBuildPropsLookup.GetDirectoryBuildPropsPath(paths, @"C:\dir\proj\project.csproj");
+        
+        Assert.Null(directoryBuildProps);
+    }
+
+    [Theory]
+    [InlineData("/home/usr/repo/tests/testProj/testProj.csproj", "/home/usr/repo/tests/Directory.Build.props")]
+    [InlineData("/home/usr/repo/tool/tool.csproj", "/home/usr/repo/Directory.Build.props")]
+    [InlineData("/home/usr/repo/src/proj/project.csproj", "/home/usr/repo/src/proj/Directory.Build.props")]
+    public void DirectoryBuildPropsLookup_WillReturnPropsFile_ForAbsolutePaths_GnuLike(string projectPath, string expectedDirectoryBuildPropsFilePath)
+    {
+        var paths = new[]
+        {
+            "/home/usr/repo/project.sln",
+            "/home/usr/repo/Directory.Build.props",
+            "/home/usr/repo/src/Directory.Build.props",
+            "/home/usr/repo/src/proj/project.csproj",
+            "/home/usr/repo/src/proj/Directory.Build.props",
+            "/home/usr/repo/tool/tool.csproj",
+            "/home/usr/repo/tests/Directory.Build.props",
+            "/home/usr/repo/tests/testProj/testProj.csproj"
+        };
+
+        var directoryBuildProps = DirectoryBuildPropsLookup.GetDirectoryBuildPropsPath(paths, projectPath, "/home/usr/repo");
         
         Assert.Equal(expectedDirectoryBuildPropsFilePath, directoryBuildProps);
     }
